47db295ab8ac2c5edcce2d186316a4ba17a57810
gustuwirak <79978999+gustuwirak@users.noreply.github.com>
2021-03-06 23:12:11 +0700

    tugas1coba
    tugas 1 sudah selesai

---
+++ main.java
@@ -0,0 +1,19 @@
+//Ida Bagus Putu Wira Kusuma
+//A11.2019.11680
+
+public class main {
+public static void main (String args []) {
+int a = 5, b = 8, c = 6, d = 9, e = 55, f = 20, g = 15, h = 3, i = 2 ;
+
+int nomor1 = -a +b*c;
+int nomor2 = (e+d)%d;
+int nomor3 = f+(-3*a)/b;
+int nomor4 = a+g/h*i-b%h;
+
+System.out.println("a. " +nomor1);
+System.out.println("b. " +nomor2);
+System.out.println("c. " +nomor3);
+System.out.println("d. " +nomor4);
+}
+
+}
